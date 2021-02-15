b1881db1bc6fa891ae4fc7ea38b90f2e3f660439
Almuzaini <62063512+YAlmuzaini@users.noreply.github.com>
2021-02-15 10:01:29 +0300

    v41

--- js/newTestion.js
+++ js/newTestion.js
@@ -16,6 +16,31 @@ $("#someSwitchOptionInfo").change(function () {
     }
 });
 
+// // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+// window.onscroll = function () { scrollFunction() };
+
+// function scrollFunction() {
+//     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+//         document.getElementById("navbar").style.padding = "30px 10px";
+//         document.getElementById("logo").style.fontSize = "25px";
+//     } else {
+//         document.getElementById("navbar").style.padding = "80px 10px";
+//         document.getElementById("logo").style.fontSize = "35px";
+//     }
+// }
+
+$(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1000) {
+            $('.header-logo .logo-normal').attr('src', 'img/image003.png');
+        }
+        if ($(this).scrollTop() < 1000) {
+            $('.header-logo .logo-normal').attr('src', 'img/logo1.svg');
+        }
+    })
+});
+
+
 // $('.sign-in__button').click(function (e) {
 //     e.preventDefault();
 //     $(this).addClass('active');
